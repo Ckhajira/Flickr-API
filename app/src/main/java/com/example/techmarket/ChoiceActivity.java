15913b76f9f8396dff31e0973b9e20680d3c8031
Ckhajira <christopher.khajira@student.moringaschool.com>
2021-12-06 06:03:31 +0300

    This commit adds the choiceactivity and its layout

--- app/src/main/java/com/example/techmarket/ChoiceActivity.java
+++ app/src/main/java/com/example/techmarket/ChoiceActivity.java
@@ -1,4 +1,14 @@
 package com.example.techmarket;
 
-public class ChoiceActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChoiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choice);
+    }
 }
